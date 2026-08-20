0b2d075abdb73f2bc4de65424fcbbca125b2696e
pranav <pranav@emmvee.in>
2026-08-20 18:05:29 +0530

    readme

--- app/main.py
+++ app/main.py
@@ -14,7 +14,11 @@ app.include_router(comments.router)
 app.include_router(users.router)
 
 
-FRONTEND_FILE = Path(__file__).resolve().parent.parent / "frontend" / "index.html"
+FRONTEND_FILE = (
+    Path(__file__).resolve().parent.parent
+    / "frontend"
+    / "index.html"
+)
 
 
 @app.get("/", include_in_schema=False)
